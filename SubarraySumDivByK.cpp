aaf72c7574e0316279e887147dcd228c1d2a3897
RahulDadhich26 <111578507+RahulDadhich26@users.noreply.github.com>
2024-07-17 18:09:15 +0530

    updated

--- SubarraySumDivByK.cpp
+++ SubarraySumDivByK.cpp
@@ -1,20 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std ;
-int subarraysDivByK(vector<int>& A, int K) {
-        int n = A.size();
-        vector<int>prefix(n+1,0);
-        for(int i=1;i<=n;i++){
-            prefix[i] = prefix[i-1] + A[i-1];
+ int subarraysDivByK(std::vector<int>& nums, int k) {
+        // Initialize count of subarrays, prefix sum, and hash map for remainders
+        int count = 0;
+        int prefixSum = 0;
+        std::unordered_map<int, int> prefixMap;
+        prefixMap[0] = 1; // To handle subarrays that start from the beginning
+
+        for (int num : nums) {
+            // Update prefix sum
+            prefixSum += num;
+            
+            // Calculate the remainder of the prefix sum divided by k
+            int mod = prefixSum % k;
+            
+            // Adjust negative remainders to be positive
+            if (mod < 0) {
+                mod += k;
+            }
+            
+            // If this remainder has been seen before, it means there are subarrays ending here that are divisible by k
+            if (prefixMap.find(mod) != prefixMap.end()) {
+                count += prefixMap[mod];
+                prefixMap[mod] += 1;
+            } else {
+                prefixMap[mod] = 1;
+            }
         }
-        vector<int>count(K,0);
-        for(int i=0;i<=n;i++){
-            count[(prefix[i]%K + K)%K]++;
-        }
-        int ans = 0;
-        for(int i=0;i<K;i++){
-            ans += (count[i]*(count[i]-1))/2;
-        }
-        return ans;
+        
+        return count; // Total number of subarrays divisible by k
     }
 int main(){
     vector<int>A = {4,5,0,-2,-3,1};
